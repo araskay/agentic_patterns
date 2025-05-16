8477d62840ac1981d41a7d345f87f4e8ac7976ab
Aras Kayvan <43323517+araskay@users.noreply.github.com>
2025-05-16 20:07:00 +0000

    prompt chaining essay writer example

--- README.md
+++ README.md
@@ -3,6 +3,12 @@ Sample implementations of common agentic and non-agentic patterns from scratch
 
 # Non-agentic patterns
 ## 1. Prompt chaining
+The output of one LLM sequentially feeds into the input of the next.
+
+`prompt_chaining_essay_writer.py` contains an example where prompt chaining is used to compose as essay using the following workflow:
+1. create an outline
+2. expand the outline by adding content to each section
+3. write an essay based on the expanded outline
 
 ## 2. Routing
 
---
+++ prompt_chaining_essay_writer.py
@@ -0,0 +1,118 @@
+
+from openai import OpenAI
+from dotenv import load_dotenv
+from typing import List
+
+load_dotenv()
+
+class EssayWriter:
+    """
+    workflow:
+    1. create an outline
+    2. expand the outline by adding content to each section
+    3. write an essay based on the expanded outline
+    """
+    def __init__(self):
+        self.client = OpenAI()
+
+    def _get_response(self, input: List, model_name: str="gpt-4o-mini") -> List:
+        """
+        Get a response from the OpenAI Response API.
+        """
+        response = self.client.responses.create(
+            model=model_name,
+            input=input
+        )
+        return response.output_text
+    
+    def _create_outline(self, topic: str) -> str:
+        """
+        Create an outline for the essay.
+        """
+        developer_prompt = """
+            You are an AI assistant that helps users write essays.
+            You will create an outline for the essay based on the topic provided.
+            The outline should be formatted as a list of sections and subsections
+            marked with numbers (1., 2., 3., etc.).
+            Include a short (1-2 sentence) description of each section.
+            Output the outline in markdown format.
+        """
+        input = [
+            {
+                "role": "developer",
+                "content": developer_prompt
+            },
+            {
+                "role": "user",
+                "content": f"Create an outline for an essay on the topic: {topic}"
+            }
+        ]
+        response = self._get_response(input=input)
+        return response
+
+    def _expand_outline(self, outline: str) -> str:
+        """
+        Expand the outline by adding content to each section.
+        """
+        developer_prompt = """
+            You are an AI assistant that helps users write essays.
+            You are provided with an outline, which will expand by adding content to each section.
+            The content should be relevant to the topic and well-structured.
+            Output the expanded outline in markdown format.
+        """
+        input = [
+            {
+                "role": "developer",
+                "content": developer_prompt
+            },
+            {
+                "role": "user",
+                "content": f"Expand the following outline: ```{outline}```"
+            }
+        ]
+        response = self._get_response(input=input)
+        return response
+    
+    def _write_essay_based_on_expanded_outline(self, expanded_outline: str) -> str:
+        """
+        Write an essay based on the expanded outline.
+        """
+        developer_prompt = """
+            You are an AI assistant that helps users write essays.
+            You are provided with an expanded outline,
+            which you will use to write an essay based on it.
+            The essay should be coherent and NOT in bullet-point format.
+            Output the essay in markdown format.
+        """
+        input = [
+            {
+                "role": "developer",
+                "content": developer_prompt
+            },
+            {
+                "role": "user",
+                "content": f"Write an essay based on the following expanded outline: ```{expanded_outline}```"
+            }
+        ]
+        response = self._get_response(input=input)
+        return response
+    
+    def write_essay(self, topic: str, verbose: bool = False) -> str:
+        """
+        Use prompt chaining workflow to write an essay based on the topic provided.
+        """
+        outline = self._create_outline(topic=topic)
+        if verbose:
+            print(f"\nOutline:\n {outline}")
+        expanded_outline = self._expand_outline(outline=outline)
+        if verbose:
+            print(f"\nExpanded Outline:\n {expanded_outline}")
+        essay = self._write_essay_based_on_expanded_outline(expanded_outline=expanded_outline)
+        return essay
+
+
+if __name__ == "__main__":
+    essay = EssayWriter().write_essay(topic="Accelerated magnetic resonance imaging (MRI)", verbose=True)
+    print("\nEssay:\n")
+    print(essay)
+
